d17c056cf8e47624e32cee9e2235f68b1a9a001b
envteh <envteh.group@gmail.com>
2026-06-11 22:19:05 +0700

    fix: manual chunks vite config & compress image

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,24 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
   ],
   base: "/",
+  build: {
+    rolldownOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom")
+          ) {
+            return "vendor-react";
+          }
+          if (id.includes("node_modules/framer-motion")) {
+            return "vendor-motion";
+          }
+          if (id.includes("node_modules/lucide-react")) {
+            return "vendor-lucide";
+          }
+        },
+      },
+    },
+  },
 });
